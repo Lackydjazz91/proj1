a6387351f7ef641c7487550781f6bb0c86a8507b
aleksey aleksin <lackydjazz91@rambler.ru>
2025-10-15 17:25:44 +0300

    Изменена основная логика Добавлен новый класс

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
-        System.out.println("111111");
+        System.out.println("11111121");
     }
 }
