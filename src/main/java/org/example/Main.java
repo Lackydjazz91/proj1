3f4712639624f4071bffc7066a2ea4087f6979b9
aleksey aleksin <lackydjazz91@rambler.ru>
2025-10-16 16:20:22 +0300

    Изменили логику main и doSomething

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,14 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("--------");
         System.out.println("Hello and welcome!");
         System.out.println("11111121");
     }
 
     public static void doSomething(){
+        System.out.println("0");
         System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
+        System.out.println("1");
     }
 }
