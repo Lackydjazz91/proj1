8142ac53ca4ffb8b925e3932e652ae6cc603751d
aleksey aleksin <lackydjazz91@rambler.ru>
2025-10-16 16:55:27 +0300

    Добавлен вызов метода doSomething

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("--------");
         System.out.println("Hello World!");
         System.out.println("11111121");
+        doSomething();
         System.out.println("-------");
     }
 
